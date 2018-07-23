7f59382ff359f68f2066136ff6399522e6f6847b
Nathan Bougeno <bougeno.nathan@gmail.com>
2018-07-23 14:51:53 -0500

    Changed default app to todo list and added input box and button

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -6,13 +6,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className='App-header'>
+          <h1 className='App-title'>To-Do List</h1>
+          <input className='InputBox'></input>
+          <button className='Button'>Add</button>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='ListArea'>
+          <ul className='List'>
+            <li>test item</li>
+          </ul>
+        </div>
       </div>
     );
   }
