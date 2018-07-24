9ea0e0494c6eb9f91a2cca06b233ca08b3454b2d
Nathan Bougeno <bougeno.nathan@gmail.com>
2018-07-24 13:58:38 -0500

    added text length validator to input box

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = {value: ''};
+    this.state = {value: '', valid: true};
 
     this.handleChange = this.handleChange.bind(this);
     this.addItem = this.addItem.bind(this);
@@ -20,27 +20,41 @@ class App extends Component {
   clearList(e){
     e.preventDefault();
     this.itemsList = [];
-    this.setState({value: ''});
+    this.setState({value: '', valid: true});
   }
 
   addItem(e){
     e.preventDefault();
     let item = this.state.value;
-    this.itemsList.push(item);
-    this.setState({value: ''});
+    this.validateItem(item);
+    if(this.state.valid){  
+      this.itemsList.push(item);
+      this.setState({value: ''});
+    }
+  }
+
+  validateItem(item){
+    if(item.length <= 0){
+      this.setState({valid: false});
+    }
   }
 
   render() {
 
     const items = this.itemsList.map((item) => {
-      return(<li>{item}</li>)
+      return(<li key={item}>{item}</li>)
     });
+
+    const warning = this.state.valid;
 
     return (
       <div className="App">
         <div className='App-header'>
+          <h1 className='App-title'>To-Do List</h1>
+          {!warning && 
+            <p className='Warning'>Please enter at least one character</p>
+          }
           <form>
-            <h1 className='App-title'>To-Do List</h1>
             <input type='text' className='InputBox'id='listInput' value={this.state.value} onChange={this.handleChange}/><br />
             <button className='Button' onClick={this.addItem}>Add Item</button>
             <button className='Button' onClick={this.clearList}>Clear List</button>
