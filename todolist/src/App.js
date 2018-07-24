73bff31fa0e2a06f2aa04e80be319a55203af12b
Nathan Bougeno <bougeno.nathan@gmail.com>
2018-07-24 13:05:28 -0500

    forgot to save

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
   
   itemsList=[];
+
+  handleChange(e){
+    this.setState({value: e.target.value});
+  }
   
   addItem(e){
-    this.itemsList.push(document.getElementById('listInput').value);
+    this.itemsList.push(this.state.value);
+    this.setState({value: ''});
   }
 
   render() {
@@ -20,7 +32,7 @@ class App extends Component {
         <div className='App-header'>
           <form>
             <h1 className='App-title'>To-Do List</h1>
-            <input type='text' className='InputBox' id='listInput'/><br />
+            <input type='text' className='InputBox'id='listInput' value={this.state.value} onChange={this.handleChange}/><br />
             <button className='Button' onClick={this.addItem}>Add Item</button>
             <button className='Button'>Clear List</button>
           </form>
