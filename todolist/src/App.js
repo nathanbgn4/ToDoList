0575bf7e3e38fcf68542eeaeda793b3ff6f13819
Nathan Bougeno <bougeno.nathan@gmail.com>
2018-07-24 13:05:09 -0500

    progress is slow

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -2,30 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  itemList=[];
   
-  addItem(){
-    this.itemList.push(document.getElementById('listInput').value);
-    document.getElementById('listInput').value = '';
+  itemsList=[];
+  
+  addItem(e){
+    this.itemsList.push(document.getElementById('listInput').value);
   }
 
   render() {
+
+    const items = this.itemsList.map((item) => 
+      <li>{item}</li>  
+    );
+
     return (
-      <body className="App">
+      <div className="App">
         <div className='App-header'>
           <form>
             <h1 className='App-title'>To-Do List</h1>
-            <input type='text' className='InputBox'id='listInput' /><br />
-            <button className='Button' onCLick={this.addItem}>Add Item</button>
+            <input type='text' className='InputBox' id='listInput'/><br />
+            <button className='Button' onClick={this.addItem}>Add Item</button>
             <button className='Button'>Clear List</button>
           </form>
         </div>
         <div className='ListArea'>
           <ul className='List'>
-            <li>test item</li>
+            {items}
           </ul>
         </div>
-      </body>
+      </div>
     );
   }
 }
