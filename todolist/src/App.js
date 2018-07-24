9cca32875f6393ad7a0669921eaaee7fe8f460a9
Nathan Bougeno <bougeno.nathan@gmail.com>
2018-07-24 11:49:18 -0500

    Made some minor css changes, removed unneeded logo, adjusted input form

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  itemList=[];
+  
+  addItem(){
+    this.itemList.push(document.getElementById('listInput').value);
+    document.getElementById('listInput').value = '';
+  }
+
   render() {
     return (
       <body className="App">
-        <header className='App-header'>
-          <h1 className='App-title'>To-Do List</h1>
-          <input className='InputBox'></input>
-          <button className='Button'>Add</button>
-        </header>
+        <div className='App-header'>
+          <form>
+            <h1 className='App-title'>To-Do List</h1>
+            <input type='text' className='InputBox'id='listInput' /><br />
+            <button className='Button' onCLick={this.addItem}>Add Item</button>
+            <button className='Button'>Clear List</button>
+          </form>
+        </div>
         <div className='ListArea'>
           <ul className='List'>
             <li>test item</li>
